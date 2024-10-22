cacbbb321e8571614576f8a7801fef61c0d5cd44
Your Name <you@example.com>
2024-10-23 00:21:35 +0300

    first commit

---
+++ app/(site)/docs/page.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Docs Page -***** ",
+  description: "This is project",
+};
+
+export default function DocsPage() {
+  return (
+    <>
+      <section className="pt-24 pb-16 md:pb-20 md:pt-28 lg:pb-24 lg:pt-32">
+      
+      </section>
+    </>
+  );
+}
